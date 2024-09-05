07068c930c155f140f47e6de674180543c58ac95
Adailton Lucas <57913921+AdailtonDumer@users.noreply.github.com>
2024-09-05 20:31:08 -0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Adailton Lucas on 05/09/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
